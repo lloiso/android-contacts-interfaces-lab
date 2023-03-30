dc1247f827e39a81787f69967e26428e790b9586
dmitrii <llosio@mail.ru>
2023-03-30 19:05:28 +0300

    medium commit

--- app/src/main/java/ru/yandex/practicum/contacts/utils/android/Debouncer.java
+++ app/src/main/java/ru/yandex/practicum/contacts/utils/android/Debouncer.java
@@ -8,15 +8,18 @@ import ru.yandex.practicum.contacts.presentation.main.MainViewModel;
 
 import androidx.annotation.NonNull;
 
-public class Debouncer {
+public class Debouncer implements OnDebounceListener {
+
 
     private static final int MESSAGE_ID = 1;
     private static final int DELAY = 500;
+    private final OnDebounceListener listener;
 
-    private final MainViewModel viewModel;
+//    private final MainViewModel viewModel;
 
-    public Debouncer(MainViewModel viewModel) {
-        this.viewModel = viewModel;
+    public Debouncer(OnDebounceListener listener) {
+        this.listener = listener;
+//        final Debouncer debouncer = new Debouncer(this);
     }
 
     private final Handler handler = new Handler(Looper.getMainLooper()) {
@@ -39,6 +42,10 @@ public class Debouncer {
 
     // выполнить действие по прошествии 500мс, если нового события в течении 500мс не было отправлено
     private void doOnDebounce() {
-        viewModel.search();
+        listener.onDebounce();
+    }
+    @Override
+    public void onDebounce() {
+
     }
 }
